b0bb9d84df7e1297a2fb795b9b3911145bbf9f39
Damon L. Montague Jr <damon@montague.im>
2023-08-07 14:45:39 -0400

    getting rid of the for components

--- src/components/SettingPopup.tsx
+++ src/components/SettingPopup.tsx
@@ -1,9 +1,7 @@
-import {Accessor, Component, createSignal, For, JSX, Setter} from "solid-js";
-import { IoCogSharp } from "solid-icons/io";
+import {Accessor, Component, Setter} from "solid-js";
 import {IFont, ITheme} from "~/constants";
-import {BiRegularCog} from "solid-icons/bi";
 import {TbPaint} from "solid-icons/tb";
-import {BsAt, BsGear, BsPencil, BsSendPlusFill} from "solid-icons/bs";
+import {BsGear, BsPencil} from "solid-icons/bs";
 
 interface SettingsProps {
     themes: ITheme[];
@@ -37,14 +35,16 @@ const Settings: Component<SettingsProps> = (props) => {
                     </label>
                     <select
                         value={props.theme()}
-                        onChange={(e) => props.setTheme(e.target.value)}
+                        onChange={(e) => {
+                            props.setTheme(e.target.value)
+                            props.onButtonClick()
+                        }}
                         class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white dark:bg-gray-800 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     >
-                        <For each={props.themes}>
-                            {(themeOption) => (
-                                <option value={themeOption.name}>{themeOption.displayName}</option>
-                            )}
-                        </For>
+
+                        {props.themes.map((themeOption) => (
+                            <option value={themeOption.name}>{themeOption.displayName}</option>
+                        ))}
                     </select>
                 </div>
                 <div class="px-4 py-2">
@@ -53,14 +53,15 @@ const Settings: Component<SettingsProps> = (props) => {
                     </label>
                     <select
                         value={props.font()}
-                        onChange={(e) => props.setFont(e.target.value)}
+                        onChange={(e) => {
+                            props.setFont(e.target.value)
+                            props.onButtonClick()
+                        }}
                         class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white dark:bg-gray-800 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
                     >
-                        <For each={props.fonts}>
-                            {(themeOption) => (
-                                <option value={themeOption.name}>{themeOption.displayName}</option>
-                            )}
-                        </For>
+                        {props.fonts.map((fontOption) => (
+                            <option value={fontOption.name}>{fontOption.displayName}</option>
+                        ))}
                     </select>
                 </div>
             </div>
