bbfaaf306ded01fcf04f7c6d00c897e1e0fb6410
Damon L. Montague Jr. <damon@montague.im>
2024-04-22 18:18:52 -0400

    update age func

--- src/routes/about.tsx
+++ src/routes/about.tsx
@@ -1,16 +1,25 @@
 import Description from "~/components/Description";
-import {createEffect, createSignal} from "solid-js";
 import {Meta, Title} from "solid-start";
 
 export default function About() {
     const age = new Date('2001-02-02');
+
+    function getAge() {
+
+        const now = new Date();
+        const _age = now.getFullYear() - age.getFullYear();
+        const m = now.getMonth() - age.getMonth();
+        if (m < 0 || (m === 0 && now.getDate() < age.getDate())) {
+            return _age - 1;
+        }
+        return _age;
+    }
 
     const randomFunFact = [
         'i do not have a favorite color',
         'i like lowercase letters',
         'i have been to universal in florida 10 times',
         'i played divisional paintball for 2 years'
-
     ]
 
     return (
@@ -26,8 +35,8 @@ export default function About() {
             <main class="flex flex-col justify-center items-center text-center space-y-3 p-6 animate-fade-in h-[85vh] md:text-2xl">
                 <h1 class="lg:text-xl text-2xl italic mb-2">hi.</h1>
                 <Description
-                    description={`im damon, a ${new Date().getFullYear() - age.getFullYear()} year old software engineer based in the usa`}
-                    highlightedWords={['software', '22', 'year', 'old', 'engineer', 'usa']}/>
+                    description={`im damon, a ${getAge()} year old software engineer based in the usa`}
+                    highlightedWords={['software', `${getAge()}`, 'year', 'old', 'engineer', 'usa']}/>
                 <Description description={"im currently working at t rowe price working on emerging technologies"}
                              highlightedWords={['t', 'rowe', 'price', 'emerging', 'technologies']}/>
                 <Description
